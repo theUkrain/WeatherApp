0defa30a6585ce682259da74bb896d78bd8cfdf5
theUkrain <nikitarozhkovv@gmail.com>
2025-08-01 21:40:05 +0200

    Hello git test2

--- weather_app/src/main/java/weather_application/Main.java
+++ weather_app/src/main/java/weather_application/Main.java
@@ -3,5 +3,6 @@ package weather_application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello gittttt!");
+        System.out.println("test 2");
     }
 }
